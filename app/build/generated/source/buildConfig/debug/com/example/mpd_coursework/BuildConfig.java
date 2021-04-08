1a2e9751c2dd55790c2905aff6362d3007094263
dejwkubikson <44845403+dejwkubikson@users.noreply.github.com>
2021-04-08 14:28:09 +0100

    Build

---
+++ app/build/generated/source/buildConfig/debug/com/example/mpd_coursework/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.mpd_coursework;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.mpd_coursework";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from the variant API
+  public static final String MAPS_API_KEY = "AIzaSyAyQdK0qPWC_4IARoVuHodlknEnGpOjJcA";
+}
